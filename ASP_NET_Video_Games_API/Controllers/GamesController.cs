f50aa528066a27fbccd3c5dd89256a1c1d6529da
mayragann <mayragann@gmail.com>
2022-05-12 14:44:50 -0400

    Created Get End Points to Get Games by ID and Get full List of Games by ID

---
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -0,0 +1,35 @@
+﻿using ASP_NET_Video_Games_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Video_Games_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GamesController(ApplicationDbContext context)
+        { 
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetVideoGames() 
+        {
+            var videoGamePublisher = _context.VideoGames.ToList();
+            
+            return Ok(videoGamePublisher);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetGamesById(int id) 
+        
+        {
+
+            var videoGames = _context.VideoGames.Where(vg => vg.Id == id);
+            return Ok(videoGames);
+        }
+    }
+}
